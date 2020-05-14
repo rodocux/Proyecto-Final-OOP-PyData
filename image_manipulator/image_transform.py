d8e27a62092ec567c67ac753d4e10962079f8933
Rodolfo Catunta <rodolfo.catunta.uturunco@gmail.com>
2020-05-13 20:37:29 -0400

    Functions to Transform Geometrically an image

--- image_manipulator/image_transform.py
+++ image_manipulator/image_transform.py
@@ -1,6 +1,7 @@
 import cv2
+import numpy as np
 
-class ImageTransform:
+class ImageColor:
 
 	@classmethod
 	def gray_scale(self,img):
@@ -14,3 +15,32 @@ class ImageTransform:
 		"""Convierte una imagen en color a una imagen HSV"""
 		hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 		return hsv_img
+
+
+class ImageTransform:
+
+	@classmethod
+	def resize(self, img, percent_size):
+		"""Redimensiona una imagen al porcentaje dado"""
+		new_width = int(img.shape[1] * percent_size / 100)
+		new_height = int(img.shape[0] * percent_size / 100)
+		new_dimension = (new_width, new_height)
+		resize_img = cv2.resize(img, new_dimension, interpolation = cv2.INTER_AREA)
+		return resize_img
+
+	@classmethod
+	def traslation(self, img, new_x, new_y):
+		"""Realiza una traslacion de una imagen a las nuevas coordenadas
+		x e y"""
+		rows,cols = img.shape[0:2]
+		matrix = np.float32([[1,0,new_x],[0,1,new_y]])
+		tras_img = cv2.warpAffine(img,matrix,(cols,rows))
+		return tras_img
+
+	@classmethod
+	def rotation(self, img, grades):
+		"""Realiza una rotacion de una imagen cierta cantidad de grados"""
+		rows,cols = img.shape[0:2]
+		M = cv2.getRotationMatrix2D((cols/2,rows/2),grades,1)
+		rotate_img = cv2.warpAffine(img, M, (cols,rows))
+		return rotate_img
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import cv2
 from image_manipulator.image_storage import ImageStorage
-from image_manipulator.image_transform import ImageTransform
+from image_manipulator.image_transform import *
 
 
 if __name__ == '__main__':
     img = ImageStorage.read_image("images/profile.jpg")
     
-    img_gray = ImageTransform.gray_scale(img)
+    img_gray = ImageColor.gray_scale(img)
     ImageStorage.save_img(img=img_gray, name_img="images/profile_gray.jpg")
 
-    img_hsv = ImageTransform.hsv(img)
+    img_hsv = ImageColor.hsv(img)
     ImageStorage.save_img(img=img_hsv, name_img="images/profile_hsv.jpg")
+
+    image_50 = ImageTransform.resize(img, 50)
+    ImageStorage.save_img(img = image_50, name_img="images/profile_50.jpg")
+
+    image_tras = ImageTransform.traslation(img, 200, 150)
+    ImageStorage.save_img(img = image_tras, name_img="images/profile_tras.jpg")
+
+    image_rot = ImageTransform.rotation(img, 45)
+    ImageStorage.save_img(img = image_rot, name_img="images/profile_rot.jpg")
+
+    print("All transformations done")
